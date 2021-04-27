bde9f48e1c54e6e0cdb96f05ecd2156affa00762
kasul <sumitpujari199723@gmail.com>
2021-04-27 14:27:52 +0530

    Record current time in hours and mins

--- input.py
+++ input.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+
 def task(n):
-    
+    now=datetime.now()
     print("Enter the Today's Task Here:- ")
     i=0
     for i in range(1,n):
         t2=str(input(i))
         if t2=="exit":
             break
-        print("Task",i,":-",t2)
+        print("Task",i,":-",t2,now.strftime("%H:%M"))
 task(1000)
