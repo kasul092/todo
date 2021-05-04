015eef6a226104bdcf48990bf95772845f2c1a77
kasul <sumitpujari199723@gmail.com>
2021-05-04 11:24:56 +0530

    Python click colour output

--- simpleclick2.py
+++ simpleclick2.py
@@ -1,10 +1,8 @@
 import click
 
 @click.command()
-@click.option('--name','-n', prompt='Your name', help='Provide Your Name')
-@click.option('--number','-na', type = int , prompt='Your number', help='Provide Your Mobile Number')
-def hello(name, number):
-    click.echo(f'my name is {name} a number {number}')
+def hello():
+    click.secho(f'hello there', fg='red', bold=True)
 
 if __name__=='__main__':
     hello()
