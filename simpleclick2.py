94911b0dc6703349cbcb498cf9e2cc4b85bcff6d
kasul <sumitpujari199723@gmail.com>
2021-05-04 10:06:17 +0530

    Python click option name

--- simpleclick2.py
+++ simpleclick2.py
@@ -1,13 +1,9 @@
 import click
-from operator import mul
-from functools import reduce 
 
 @click.command()
-@click.argument('val', type = int, nargs=-1)
-def hello(val):
-    click.echo(f'The sum is {sum(val)}')
-    click.echo(f'The product is {reduce(mul, val, 1)}')
-
+@click.option('--name','-n')
+def hello(name):
+    click.echo(f'my name is {name}')
 
 if __name__=='__main__':
     hello()
