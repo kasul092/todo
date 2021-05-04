096f20a8e6c8e63c20de8ee2d7d5a12090652d3b
kasul <sumitpujari199723@gmail.com>
2021-05-04 11:04:14 +0530

    Python click prompt for value

--- simpleclick2.py
+++ simpleclick2.py
@@ -1,9 +1,10 @@
 import click
 
 @click.command()
-@click.option('--name','-n')
-def hello(name):
-    click.echo(f'my name is {name}')
+@click.option('--name','-n', prompt='Your name', help='Provide Your Name')
+@click.option('--number','-na', type = int , prompt='Your number', help='Provide Your Mobile Number')
+def hello(name, number):
+    click.echo(f'my name is {name} a number {number}')
 
 if __name__=='__main__':
     hello()
