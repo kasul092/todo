554cf7431d94dbaafb401da81f14c93944a9c7ea
kasul <sumitpujari199723@gmail.com>
2021-05-05 08:25:13 +0530

    Python click flags 1

--- simpleclick2.py
+++ simpleclick2.py
@@ -1,8 +1,13 @@
 import click
 
 @click.command()
-def hello():
-    click.secho(f'hello there', fg='red', bold=True)
+@click.option('--blue', is_flag=True, help='message in blue colour')
+def hello(blue):
+
+    if blue:
+        click.secho(f'hello there', fg='blue')
+    else:
+        click.secho(f'hello there')
 
 if __name__=='__main__':
     hello()
