459ba86e1b23b5f6ef199eee3266a5699dcdf2a4
kasul <sumitpujari199723@gmail.com>
2021-05-05 12:21:11 +0530

    Python click option tuple

--- simpleclick2.py
+++ simpleclick2.py
@@ -1,15 +1,9 @@
 import click
 
 @click.command()
-@click.argument('word')
-@click.option('--shout/--no-shout', default=False, help='message in upper case')
-def hello(word, shout):
-    
-
-    if shout:
-        click.secho(word.upper(), fg='yellow', bold=True)
-    else:
-        click.secho(word, fg='yellow', bold=True)
+@click.option('--data', required = True, type=(str, int))
+def hello(data):
+        click.echo(f'name={data[0]} age={data[1]}') 
     
 if __name__=='__main__':
     hello()
