906bc90e9ea5e0b745b16d2f39af3a1fbea00e30
kasul <sumitpujari199723@gmail.com>
2021-05-14 13:56:38 +0530

    setup tool for crating todo package

---
+++ OneDrive/Desktop/python_programs/my_folder/new_folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = "Todo",
+version = "0.1",
+description = "This is command line manager tool for writing daily tasks and track the progress.",
+Long_description = '''Todo is a command line manager tool which keep the record of any person or any group's activities in the form of tasks.
+                    The user will give the input of his tasks. This application will save it with the date and time.
+                    Update and delete facility also availble.
+                    The user will tracck their peroformance also and compare it.''',
+author = "Kasul corporation pvt. ltd.",
+packages = ['todo'],
+install_requires = [])
