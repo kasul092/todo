e3f54604f1b62c1723f1e9d1cc4bec47b8833e9c
kasul <sumitpujari199723@gmail.com>
2021-06-10 17:05:30 +0530

    fix bug of update feature

--- todo/database.py
+++ todo/database.py
@@ -5,7 +5,6 @@ class DatabaseConnection():
 
     def __init__(self):
         self.con = sqlite3.connect("test.db")
-        print("Database created.")
         self.cur = self.con.cursor()
 
         self.cur.execute(
@@ -13,7 +12,6 @@ class DatabaseConnection():
             (t_id INTEGER primary key AUTOINCREMENT, task TEXT)""" 
         )
         self.con.commit()
-        print("Table created.")
 
     def add_task(self, task):
         self.task = task
@@ -23,27 +21,21 @@ class DatabaseConnection():
         self.con.commit()
         print("task added successfully.")
 
-    def show_task(self,t_id):
-        self.t_id = t_id
+    def show_task(self):
         self.cur.execute(
             """SELECT * FROM myroutine""")
         fat = self.cur.fetchall()
-        # for task in fat:
-        #     print(task)
-        s = tabulate(fat,headers= ['ID', 'Task', 'Updated Task'],tablefmt='fancy_grid')
+        s = tabulate(fat,headers= ['ID', 'Task'],tablefmt='fancy_grid')
         print(s)
 
-    def update_task(self, t_id, task):
-        self.t_id = t_id
+    def update_task(self,t_id,task):
         self.task = task
-        # self.cur.execute(
-        #     """ SELECT task FROM myroutine WHERE t_id=?""",
-        #     (self.t_id))
+        self.t_id = t_id
         self.cur.execute(
             """UPDATE myroutine
                 SET task = ?
                 WHERE t_id = ?""",
-                (self.task, self.t_id))
+                (self.task,self.t_id))
         self.con.commit()
         print("Task updated successfully.")
                   
--- todo/task.py
+++ todo/task.py
@@ -8,9 +8,12 @@ dc = DatabaseConnection()
 @click.command(help = "Add the task.")
 @click.option("--add","-a")
 def add(add):
-    task_added = dc.add_task(add)
-    if task_added:
-        print("Task added successfully.")
+    if add:
+        task_added = dc.add_task(add)
+        if task_added:
+            print("Task added successfully.")
+    else:
+        print("Please provide a Task")
 
 
 @click.command(help = "Show the task.")
@@ -21,20 +24,8 @@ def show(show):
 @click.command(help = "Update the task.")
 @click.option("--update","-u")
 def update(update):
-    # task_list = []
-    # for update_to_task in update:
-    #     task_list.append(update_to_task)
-    # t_id = task_list[0]
-    # task = task_list[1]
-    t_id = click.prompt("Enter t_id which you want to modify")
-    mod_check = dc.show_task(t_id)
-    if mod_check is None:
-        print("No records found")
-    else:
-        mod = click.prompt("Enter new task", default="None")
-        dc.update_task(t_id = t_id , task = mod)
-
-    # task_updated = dc.update_task(update,"task")
-    # if task_updated:
-    #     print("Task updated successfully.")
+    task_id = int(click.prompt("Enter t_id which you want to modify"))
+    updated_task = click.prompt("Enter new task")
+    dc.update_task(t_id = task_id , task = updated_task)
+    
     
