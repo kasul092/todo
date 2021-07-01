d7d6715a782162b936247cf443cf89996fc48833
kasul <sumitpujari199723@gmail.com>
2021-07-01 20:46:18 +0530

    add one line in setup.py and remove syntax error in readme.md file

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ with open("README.md") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name = "ToDo",
+    name = "ToDo Task Manager Tool",
     version = "0.1.0",
     description = "Todo manager tool",
     long_description = readme,
+    long_description_content_type='text/markdown',
     url = "https://github.com/kasul092/todo",
     author  = "Sumit Pujari",
     author_email = "sumitpujari199723@gmail.com",
